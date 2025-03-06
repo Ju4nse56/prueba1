2d1c5af5eb5f83155b9cf5a4f10fc01c7226954c
Ju4nse56 <jmanotasb.2005@gmail.com>
2025-03-05 19:57:19 -0500

    Update contacto.php

--- contacto.php
+++ contacto.php
@@ -1,23 +1,29 @@
 <?php
 $mensajeEnviado = false;
 $errorEnvio = false;
+$nombre = $email = $mensaje = ""; // Inicializamos las variables
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $nombre = htmlspecialchars($_POST['nombre']);
-    $email = htmlspecialchars($_POST['email']);
-    $mensaje = htmlspecialchars($_POST['mensaje']);
+    if (!empty($_POST['nombre']) && !empty($_POST['email']) && !empty($_POST['mensaje'])) {
+        $nombre = htmlspecialchars($_POST['nombre']);
+        $email = htmlspecialchars($_POST['email']);
+        $mensaje = htmlspecialchars($_POST['mensaje']);
 
-    $to = "tucorreo@gmail.com"; // Cambia esto por tu dirección de Gmail
-    $subject = "Nuevo mensaje de contacto";
-    $body = "Nombre: $nombre\nCorreo: $email\nMensaje: $mensaje";
-    $headers = "From: $email\r\n";
-    $headers .= "Reply-To: $email\r\n";
-    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+        $to = "tucorreo@gmail.com"; // Cambia esto por tu dirección de Gmail
+        $subject = "Nuevo mensaje de contacto";
+        $body = "Nombre: $nombre\nCorreo: $email\nMensaje: $mensaje";
+        $headers = "From: $email\r\n";
+        $headers .= "Reply-To: $email\r\n";
+        $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
 
-    // Intentar enviar el correo
-    if (mail($to, $subject, $body, $headers)) {
-        $mensajeEnviado = true;
+        // Intentamos enviar el correo
+        if (mail($to, $subject, $body, $headers)) {
+            $mensajeEnviado = true;
+        } else {
+            $errorEnvio = true;
+        }
     } else {
         $errorEnvio = true;
     }
 }
+?>
