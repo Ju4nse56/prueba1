586ae5c6506f925160987b0d77ec7a0f9f5d6ff6
Ju4nse56 <jmanotasb.2005@gmail.com>
2025-03-05 20:05:52 -0500

    Update contacto.php

--- contacto.php
+++ contacto.php
@@ -1,13 +1,26 @@
-<?php
-$nombre = $email = $mensaje = ""; // Variables para almacenar los datos
+class Contacto {
+    public $nombre;
+    public $email;
+    public $mensaje;
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $nombre = htmlspecialchars($_POST['nombre']);
-    $email = htmlspecialchars($_POST['email']);
-    $mensaje = htmlspecialchars($_POST['mensaje']);
-}
-    } else {
-        $errorEnvio = true;
+    public function __construct($nombre, $email, $mensaje) {
+        $this->nombre = htmlspecialchars($nombre);
+        $this->email = htmlspecialchars($email);
+        $this->mensaje = htmlspecialchars($mensaje);
+    }
+
+    public function mostrarMensaje() {
+        return "<div class='message-box'>
+                    <h2>Datos Enviados:</h2>
+                    <p><strong>Nombre:</strong> $this->nombre</p>
+                    <p><strong>Email:</strong> $this->email</p>
+                    <p><strong>Mensaje:</strong> " . nl2br($this->mensaje) . "</p>
+                </div>";
     }
 }
-?>
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $contacto = new Contacto($_POST['nombre'], $_POST['email'], $_POST['mensaje']);
+    echo $contacto->mostrarMensaje();
+}
+
