bbaa66f70d02acb892bf02735ce8120f15a204ac
Ju4nse56 <jmanotasb.2005@gmail.com>
2025-03-05 19:59:57 -0500

    Update contacto.php

--- contacto.php
+++ contacto.php
@@ -1,27 +1,11 @@
 <?php
-$mensajeEnviado = false;
-$errorEnvio = false;
-$nombre = $email = $mensaje = ""; // Inicializamos las variables
+$nombre = $email = $mensaje = ""; // Variables para almacenar los datos
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (!empty($_POST['nombre']) && !empty($_POST['email']) && !empty($_POST['mensaje'])) {
-        $nombre = htmlspecialchars($_POST['nombre']);
-        $email = htmlspecialchars($_POST['email']);
-        $mensaje = htmlspecialchars($_POST['mensaje']);
-
-        $to = "tucorreo@gmail.com"; // Cambia esto por tu dirección de Gmail
-        $subject = "Nuevo mensaje de contacto";
-        $body = "Nombre: $nombre\nCorreo: $email\nMensaje: $mensaje";
-        $headers = "From: $email\r\n";
-        $headers .= "Reply-To: $email\r\n";
-        $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
-
-        // Intentamos enviar el correo
-        if (mail($to, $subject, $body, $headers)) {
-            $mensajeEnviado = true;
-        } else {
-            $errorEnvio = true;
-        }
+    $nombre = htmlspecialchars($_POST['nombre']);
+    $email = htmlspecialchars($_POST['email']);
+    $mensaje = htmlspecialchars($_POST['mensaje']);
+}
     } else {
         $errorEnvio = true;
     }
